c9fd35ab08c4edd6868e03191a23529fd4415bda
Narek <narek.tarverdyan@bk.ru>
2016-12-27 13:48:42 +0300

    init

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,1 +1,10 @@
-console.log('hello world');
+$(document).ready(function() {
+	$('.search').click(function(event) {
+		if(!$(this).hasClass('active'))
+			$(this).addClass('active');
+	});
+	$('.close-search').click(function(event) {
+		event.stopImmediatePropagation();
+		$('.search').removeClass('active');
+	});
+});

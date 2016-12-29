58b670b4e68ce269d427e262b62a10d143a26162
Narek <narek.tarverdyan@bk.ru>
2016-12-29 15:46:15 +0300

    projects page + single page + employees page

--- dev/js/main.js
+++ dev/js/main.js
@@ -7,4 +7,23 @@ $(document).ready(function() {
 		event.stopImmediatePropagation();
 		$('.search').removeClass('active');
 	});
+	$('.single-slider__thumbs').slick({
+		infinite: true,
+		slidesToShow: 5,
+		slidesToScroll: 1
+	});
+	$('.single-slider__thumbs .img-wrap').click(function(event) {
+		var src = $(this).data('src');
+		var videoUrl = $(this).data('url');
+		if(src) {
+			$('.single-slider__view').css('background-image', 'url('+src+')');
+			$('.single-video').attr('src', '');
+		} else {
+			$('.single-video').attr('src', videoUrl);
+		}
+	});
+	var moneyFormat = wNumb({
+		decimals: 0,
+		thousand: ' ',
+	})
 });
